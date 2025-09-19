dc1af28b9e6f0fdb737c1cd901701e6e1248ca7f
kangyesol <iks4799@naver.com>
2025-09-19 11:38:58 +0900

    week03: classes02 collections01

---
+++ kotlinBasics/README.md
@@ -0,0 +1,11 @@
+# Week02 - kotlin Basics 1
+
+## Learning Objectives
+- Understand basic Kotlin syntax
+- Declare variables and function
+
+## Pratice Files
+- `MainActivity.kt` : variables, functions
+
+## How to Run
+- Click the Run button in Android Studio.(shift+F10)
---
+++ kotlinBasics/app/src/main/java/com/kotlinbasic/MainActivity.kt
@@ -0,0 +1,153 @@
+package com.kotlinbasic
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.kotlinbasic.ui.theme.KotlinBasicTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            KotlinBasicTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+//        week02Variables()
+//        week02Functions()
+//        week03Classe()
+        week03Collections()
+    }
+}
+
+private fun week03Collections(){
+    println("== Kotlin Collections ==")
+
+    val fruits = listOf("apple", "banana", "orange")
+    val mutableFruits = mutableListOf("kiwi", "watermelon")
+
+//    fruits.add("kiwi")
+    println("Fruits : $fruits")
+    mutableFruits.add("banana")
+    println("Mutable fruits : $mutableFruits")
+
+    val scores = mapOf("Kim" to 100, "Park" to 97, "Lee" to 99) // 키 to 값
+    println("Scores : $scores")
+
+    for(fruit in fruits){
+        println("I like $fruit")
+    }
+
+    scores.forEach{(name,score) -> println("$name scored $score")}
+}
+
+private fun week03Classe(){
+    println("== Kotlin Classes ==")
+
+    class Student{
+        var name: String = ""
+        var age: Int = 0
+
+        fun introduce() {
+            println("HI, I'm $name and I'm $age years old")
+        }
+    }
+    val student = Student()
+    student.name = "Mirae"
+    student.age = 21
+    student.introduce()
+
+    data class Person(val name: String, val age: Int)
+
+    val person1 = Person("Kim",23)
+    val person2 = Person("Kim",23)
+
+    println("Person1 : $person1")
+    println("Equal? ${person1 == person2}")
+}
+
+private fun week02Functions(){
+//    println("Week 02 : Functions")
+//
+//    fun greet(name: String) = "Hello, $name!"
+//
+//    println(greet("Android developer"))
+
+    println("== kotlin Functioins ==")
+
+    fun greet(name: String): String {
+        return "Hello, $name"
+    }
+
+    fun add(a: Int, b: Int) = a+b
+
+    fun introduce(name: String, age: Int = 19){ // 매개변수 디폴트 값 지정 가능
+        println("My name is $name and I'm $age years old")
+    }
+
+    println(greet("kotline"))
+    println("Sum: ${add(5,-71)}")
+    introduce("Kim",7)
+    introduce("Park")
+}
+
+private fun week02Variables(){
+//    println("Week 02 : Variables")
+//
+//    val courseName = "Mobile Programming"
+//    //courseName = "IoT Programming"
+//    var week = 1
+//    week = 2
+//    println("Course : $courseName")
+//    println("Week : $week")
+    println("== kotlin Variables ==")
+
+    //val(immutable) vs var(mutable)
+    val name = "Android"
+    var version = 8
+
+    println("Hello $name $version")
+
+    val age: Int = 24
+    val height: Double = 177.7
+    val isStudent: Boolean = false
+
+    println("Age: $age, Height: $height, Student: $isStudent")
+
+//    var nickname: String = null
+    var nickname: String? = null
+    nickname = "mirae"
+    println("Nickname: $nickname ${nickname?.length}")
+
+}
+
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    KotlinBasicTheme {
+        Greeting("Android")
+    }
+}
