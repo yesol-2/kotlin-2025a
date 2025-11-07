4985de3f1fac260f031333f9bebc9b864f817c86
kys <iks4799@naver.com>
2025-11-07 12:33:39 +0900

    week09: Student List App v0.9(RecyclerView)

--- AppWeek09/app/build.gradle.kts
+++ AppWeek09/app/build.gradle.kts
@@ -34,6 +34,12 @@ android {
         jvmTarget = "11"
     }
     // ViewBinding Activate
+    // 장점
+    // 1. 타입 안정성 , 2. Null 안정성(존재하는 view만 보여줌),
+    // 3. findViewById 반복 제거 4. 성능 향상
+    // 단점
+    // 1. 빌드 시간 증가(이후에는 좋아짐), 2. 바이딩 객체 생성 필요,
+    // 3. 학습 곡선(배우기 위한 진입 장벽이 높음)
     buildFeatures{
         viewBinding = true
     }
